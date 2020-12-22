0e7eb7db94daa32e7ab5bf3c3af378d237991dec
shaheeriqbal88 <shaheeriqbal88@gmail.com>
2020-12-22 17:13:54 +0500

    Test app completed

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -7,26 +7,30 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
 
+
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.example.testapp.HelperClasses.FeaturedAdapter;
+import com.example.testapp.HelperClasses.FeaturedHelperClass;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private static RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
-    private static RecyclerView recyclerView;
-
     private DrawerLayout drawer;
+    RecyclerView featuredRecycler;
+    RecyclerView.Adapter adapter;
 
-    String test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +49,27 @@ public class MainActivity extends AppCompatActivity {
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        featuredRecycler = findViewById(R.id.recycler_view);
+        featuredRecycler();
+
+
     }
+
+    private void featuredRecycler() {
+
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+
+        ArrayList<FeaturedHelperClass> featuredLocations = new ArrayList<>();
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.new3, "TODAY NEWS", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tristique ex vel quam feugiat iaculis. Nunc vel molestie libero, vitae feugiat est. Aenean in sodales risus, ut viverra lacus."));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.new2, "TODAY NEWS", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tristique ex vel quam feugiat iaculis. Nunc vel molestie libero, vitae feugiat est. Aenean in sodales risus, ut viverra lacus."));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.new1, "TODAY NEWS", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tristique ex vel quam feugiat iaculis. Nunc vel molestie libero, vitae feugiat est. Aenean in sodales risus, ut viverra lacus."));
+
+        adapter = new FeaturedAdapter(featuredLocations);
+        featuredRecycler.setAdapter(adapter);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
